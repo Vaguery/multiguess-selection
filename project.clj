8a22689c51697a7734ee56a69c1e757630762d8c
Bill Tozier <bill@vagueinnovation.com>
2017-03-18 18:02:51 -0400

    initial setup

---
+++ project.clj
@@ -0,0 +1,5 @@
+(defproject multiguess-selection "0.0.1-SNAPSHOT"
+  :description "Cool new project to do things and stuff"
+  :dependencies [[org.clojure/clojure "1.8.0"]
+                 [proto-repl "0.3.1"]]
+  :profiles {:dev {:dependencies [[midje "1.8.3"]]}})
---
+++ test/multiguess_selection/core_test.clj
@@ -0,0 +1,3 @@
+(ns multiguess-selection.core-test
+  (:use midje.sweet)
+  (:use [multiguess-selection.core]))
