e4a4815d46ef6e86f0ec2189e89719910f7e13d9
Bill Tozier <bill@vagueinnovation.com>
2017-03-19 09:09:26 -0400

    demo code

--- src/multiguess_selection/core.clj
+++ src/multiguess_selection/core.clj
@@ -1,1 +1,101 @@
 (ns multiguess-selection.core)
+
+(defrecord Individual [genome stack guesses])
+
+
+(defn abs-error
+  "returns the absolute difference between two numbers"
+  [n1 n2]
+  (Math/abs (- n1 n2)))
+
+
+(defn depth-score-pairs
+  "given a collection ('stack') and a target value, maps this into a collection of vectors where the first item is the depth in the original stack, and the second the absolute error of the item vs the target value"
+  [stack target]
+  (let [diffs (map (partial abs-error target) stack)]
+    (reduce-kv
+      (fn [new k v] (conj new [k v]))
+      []
+      (into [] diffs))
+      ))
+
+
+(defn dominates?
+  "returns `true` if v1 strictly dominates v2 (both values are at least as small, and one or both are smaller)"
+  [v1 v2]
+  (boolean
+    (and
+      (every? true? (map <= v1 v2))
+      (some true? (map < v1 v2))
+      )))
+
+
+(defn non-dominating-pair?
+  "returns `true` if neither v1 or v2 dominates the other"
+  [v1 v2]
+  (not (or
+    (dominates? v1 v2)
+    (dominates? v2 v1))))
+
+
+(defn non-dominated-set
+  "given a collection of vectors, returns the subset of the vectors which are mutually non-dominating"
+  [vectors]
+  (reduce
+    (fn [bests v]
+      (if (some #(dominates? % v) bests)
+        bests
+        (if (some #(dominates? v %) bests)
+          (conj
+            (into #{} (remove #(dominates? v %) bests))
+            v)
+          (conj bests v))))
+    #{}
+    vectors
+    ))
+
+
+(defn best-guesses
+  "given a stack of numbers and a target, returns the non-dominated subset of items in the stack, using both depth and error (as depth-score-pairs)"
+  [stack target]
+  (non-dominated-set
+    (depth-score-pairs stack target)))
+
+
+(defn score-individual
+  "given an Individual record with a stack in it and a target number, assigns the Individual's `:guesses` field to be the subset of non-dominated items in the stack"
+  [individual target]
+  (assoc individual
+         :guesses
+         (best-guesses (:stack individual) target)))
+
+
+(defn best-overall-guesses
+  "given a collection of `Individual` records (with `:guesses` assigned), returns the set of non-dominated guess vectors appearing anywhere in the entire collection"
+  [individuals]
+  (non-dominated-set
+    (reduce
+      #(into %1 (:guesses %2)) #{} individuals)))
+
+
+(defn extract-best-guessers
+  "given a collection of individuals, this returns only those individuals whose `:guesses` contain collectively-nondominated vectors"
+  [individuals]
+  (let [bests (best-overall-guesses individuals)]
+    (group-by #(not-any? bests (:guesses %)) individuals)
+    ))
+
+
+(defn ranked-individuals
+  "given a collection of Individuals, this recursively partitions the collection in 'layers' of non-dominated vs dominated. The key is the 'degree' of domination, taking each layer out in turn and recalculating"
+  [individuals]
+  (loop [unsorted individuals
+         counter 0
+         ranks {}]
+    (let [parts (extract-best-guessers unsorted)]
+      (if (empty? (get parts true))
+        (assoc ranks counter (get parts false))
+        (recur (get parts true)
+               (inc counter)
+               (assoc ranks counter (get parts false))
+               )))))
--- test/multiguess_selection/core_test.clj
+++ test/multiguess_selection/core_test.clj
@@ -1,3 +1,90 @@
 (ns multiguess-selection.core-test
   (:use midje.sweet)
   (:use [multiguess-selection.core]))
+
+(fact "I can make an Individual record"
+  (:genome (->Individual 1 [] [])) => 1
+  (:stack (->Individual 2 [2 3] [])) => [2 3]
+  (:guesses (->Individual 3 [1 8] #{[1 8]})) =>
+    #{[1 8]}
+  )
+
+(fact "depth-score-pairs converts a stack and a target into [position abs-error] vectors"
+  (depth-score-pairs '(3) 2) => [[0 1]]
+  (depth-score-pairs '(2) 2) => [[0 0]]
+  (depth-score-pairs '(7 1) 2) => [[0 5] [1 1]]
+  (depth-score-pairs '(7 1) 7) => [[0 0] [1 6]]
+  (depth-score-pairs '(2 2) 2) => [[0 0] [1 0]]
+  (depth-score-pairs '() 2) => []
+  )
+
+
+(fact "dominates? predicate"
+  (dominates? [0 0] [1 1]) => true
+  (dominates? [2 2] [1 1]) => false
+  (dominates? [1 2] [2 1]) => false
+  (dominates? [1 2] [1 2]) => false
+  (dominates? [1 2] [1 3]) => true
+  (dominates? [1 3] [1 2]) => false
+  )
+
+
+(fact "best-guesses returns the set of non-dominated depth-score-pairs from a stack and a target value"
+  (best-guesses '(2 2) 2) => #{[0 0]}
+  (best-guesses '(1 2) 2) => #{[0 1] [1 0]}
+  (best-guesses '(5 4 3 2 1) 9) =>
+    #{[0 4]}
+  (best-guesses '(5 4 3 2 1) 5) =>
+    #{[0 0]}
+  (best-guesses '(5 4 3 2 1) 4) =>
+    #{[0 1] [1 0]}
+  (best-guesses '(5 4 3 2 1) 3) =>
+    #{[0 2] [1 1] [2 0]}
+  (best-guesses '(5 4 3 2 1) 0) =>
+    #{[0 5] [1 4] [2 3] [3 2] [4 1]}
+  (best-guesses '(5 4 5 4 5) 0) =>
+    #{[0 5] [1 4]}
+    )
+
+
+(fact "score-individual takes an Individual record with stack, and a target, and fills its :guesses with a best-guesses set"
+  (let [dude (->Individual 1 '(8 2 9 1 4 0) [])]
+    (:guesses (score-individual dude 1)) => #{[0 7] [1 1] [3 0]}
+    (:guesses (score-individual dude 2)) => #{[0 6] [1 0]}
+    (:guesses (score-individual dude 3)) => #{[0 5] [1 1]}
+    (:guesses (score-individual dude 4)) => #{[0 4] [1 2] [4 0]}
+    ))
+
+
+(fact "best-overall-guesses takes a collection of Individual records, all with best-guesses set, and returns the mutually non-dominated guess vectors"
+  (best-overall-guesses
+    [(score-individual (->Individual 1 '(0 1 2) #{}) 1)
+    (score-individual (->Individual 2 '(1 2 0) #{}) 1)]) => #{[0 0]}
+  (best-overall-guesses
+    [(score-individual (->Individual 1 '(0 1 2) #{}) 3)
+    (score-individual (->Individual 2 '(1 2 0) #{}) 3)]) => #{[0 2] [1 1]}
+  (best-overall-guesses
+    [(score-individual (->Individual 1 '(0 1 2) #{}) 7)
+    (score-individual (->Individual 2 '(0 1 1) #{}) 7)]) => #{[0 7] [1 6] [2 5]}
+  (best-overall-guesses
+    [(score-individual (->Individual 1 '(0 1 2) #{}) 1)]) => #{[0 1] [1 0]}
+  (best-overall-guesses []) => #{}
+  )
+
+
+(fact "I can rank a collection of Individuals using ranked-individuals"
+  (let [pop (repeatedly 20
+              #(score-individual
+                (->Individual
+                  (rand-int 100)
+                  (repeatedly 20 (fn [] (rand-int 1000)))
+                  #{})
+                13))]
+    (ranked-individuals
+      [(score-individual (->Individual 1 '(0 1 2) #{}) 3)
+       (score-individual (->Individual 2 '(1 2 0) #{}) 3)]) => {0
+            [{:genome 2, :guesses #{[0 2] [1 1]}, :stack '(1 2 0)}],
+          1
+            [{:genome 1, :guesses #{[0 3] [1 2] [2 1]}, :stack '(0 1 2)}]}
+    (ranked-individuals pop) => 99 ;; intentional fail
+    ))
